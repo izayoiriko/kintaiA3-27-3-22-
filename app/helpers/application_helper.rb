02236e47691f7c020845af6aa830af24066fcc76
izayoiliko <mmoursnuimneg@gmail.com>
2020-03-11 09:00:21 +0000

    3/11 header 追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   # ページごとにタイトルを返す
   def full_title(page_name = "")
     base_title = "AttendanceApp"  
-    if page_name.mpty? # boolean値
+    if page_name.empty? # boolean値
       base_title # 空の場合の処理
     else
       page_name + " | " + base_title # 文字列連結
