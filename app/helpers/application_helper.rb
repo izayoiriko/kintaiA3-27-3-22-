0ea606c68498fd3b8b5b5d73e8ed505c37a0dce0
izayoiliko <mmoursnuimneg@gmail.com>
2020-03-11 06:38:16 +0000

    3/11 application.html トップページ 追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-  
+  # ページごとにタイトルを返す
   def full_title(page_name = "")
-    base_title = "AttendanceApp"
-    if page_name.mpty?
-      base_title
+    base_title = "AttendanceApp"  
+    if page_name.mpty? # boolean値
+      base_title # 空の場合の処理
     else
-      page_name + " | " + base_title
+      page_name + " | " + base_title # 文字列連結
     end
   end
 end
