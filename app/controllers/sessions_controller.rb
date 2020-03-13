d867d70259540b89fc27a4309671985eb79279af
izayoiliko <mmoursnuimneg@gmail.com>
2020-03-13 09:25:04 +0000

    3/13 include Sessions_controller-create 追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase) # sessionの箱の中のemailを取り出す
     if user && user.authenticate(params[session][:password])
-      
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = '認証に失敗しました。'
       render :new
