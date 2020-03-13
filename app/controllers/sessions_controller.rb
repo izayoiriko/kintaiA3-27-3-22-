2a7b729b78bda5c5df4521f65fbb89ffbb8f0500
izayoiliko <mmoursnuimneg@gmail.com>
2020-03-13 12:54:21 +0000

    3/13 ログアウト機能 追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,18 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by(email: params[:session][:email].downcase) # sessionの箱の中のemailを取り出す
-    if user && user.authenticate(params[session][:password])
+    if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to user
     else
       flash.now[:danger] = '認証に失敗しました。'
       render :new
     end
+  end
+  
+  def destroy
+    log_out
+    flash[:success] = "ログアウトしました。"
+    redirect_to root_url
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,11 +5,20 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
+# セッションと@current_userを破棄します
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+  
   # 現在ログイン中のユーザーがいる場合オブジェクトを返します。
   def current_user
-    if session[:id]
-      @current_user ||= User.find_by(id:session[:id])
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
     end
   end
-      
+     
+  def logged_in?
+    !current_user.nil?
+  end
 end
