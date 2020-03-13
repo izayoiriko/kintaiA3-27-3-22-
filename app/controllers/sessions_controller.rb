7d154b91e0eeba36d067c387fb539d834d8203e8
izayoiliko <mmoursnuimneg@gmail.com>
2020-03-13 07:32:35 +0000

    3/13 session_controller-create 追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,12 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  def create
+    use = User.find_by(email: params[:session][:email].downcase) # sessionの箱の中のemailを取り出す
+    if user && user.authenticate(params[session][:password])
+    else
+      render :new
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'static_pages#top'
   get '/signup', to: 'users#new'
 
-  get    '/login', to: 'sessions/new'
+  get    '/login', to: 'sessions#new'
   post   '/login', to: 'sessions#create'
   post   '/logout', to: 'sessions#destroy'
 
