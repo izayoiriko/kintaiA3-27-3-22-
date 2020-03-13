ab3ba7f8accbdf26f41165f42ca9de504d6b2b94
izayoiliko <mmoursnuimneg@gmail.com>
2020-03-13 00:24:23 +0000

    3/13 validates 追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
                     format: {with: VALID_EMAIL_REGEX},
                     uniqueness: true
   has_secere_password
+  validates :password, presence: true, length: { minimum: 6 }
   
 end
