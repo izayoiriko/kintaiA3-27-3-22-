a1378d9af0b7480c20b99fca1667e1c5338bc8f0
izayoiliko <mmoursnuimneg@gmail.com>
2020-03-14 01:39:39 +0000

    3/14 checkbox 追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase) # sessionの箱の中のemailを取り出す
     if user && user.authenticate(params[:session][:password])
       log_in user
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = '認証に失敗しました。'
