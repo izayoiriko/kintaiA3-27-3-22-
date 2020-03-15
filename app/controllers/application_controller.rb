c847896a74c1936412966ac12fcf193b9daa43a0
izayoiliko <mmoursnuimneg@gmail.com>
2020-03-15 11:37:10 +0000

    3/15-2 10-4-1 application １か月表示切替 追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,19 +6,21 @@ class ApplicationController < ActionController::Base
   
   # ページ出力前に1か月分のデータの存在を確認・セットします。
   def set_one_month
-    @first_day = params[:date].nil?
+    @first_day = params[:date].nil? ?
     Date.current.beginning_of_month : params[:date].to_date
     @last_day = @first_day.end_of_month
     one_month = [*@first_day..@last_day]  # 対象の月の日数を代入
     # ユーザーに紐付く１か月分のレコードを検索し取得します。
-    @attendance = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
+    @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
     
-    unless one_month.count == @attendance.count # それぞれの件数（日数）が一致するか評価します。
-      ActiveRecord:: Base.transaction do # トランザクションを開始
+    unless one_month.count == @attendances.count # それぞれの件数（日数）が一致するか評価します。
+      ActiveRecord::Base.transaction do # トランザクションを開始
         # 繰り返しにより、1か月勤怠データを生成する
         one_month.each { |day| @user.attendances.create!(worked_on: day) }
       end
+      @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
     end
+    
   rescue ActiveRecord:: RecordInvalid #トランザクションによるエラーの分岐
     flash[:danger] = "ページ情報の取得に失敗しました、再アクセスしてください。"
     redirect_to root_url
