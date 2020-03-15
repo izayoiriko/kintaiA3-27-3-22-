64dd5cb35d736dadabfae764eea883a2af3277fb
izayoiliko <mmoursnuimneg@gmail.com>
2020-03-15 12:36:42 +0000

    3/15-2 退勤ボタン 追加

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -4,8 +4,8 @@ class AttendancesController < ApplicationController
     @user = User.find(params[:user_id])
     @attendance = Attendance.find(params[:id])
     
-    if @attendance.sterted_at.nil?
-      if @attendance.update_attributes(started_at: Time.current)
+    if @attendance.started_at.nil?
+      if @attendance.update_attributes(started_at: Time.current.change(sec: 0))
         flash[:info] = "おはようございます！"
       else
         flash[:danger] ="勤怠登録に失敗しました。やり直してください。"
