10c6493dc063c94e95785ecf849db6e1c95ca14d
izayoiliko <mmoursnuimneg@gmail.com>
2020-03-20 12:38:06 +0000

    3/20 出勤・退勤ボタン別表示 追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info, :edit_basic_ad, :update_basic_ad]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info, :edit_basic_ad, :update_basic_ad]
   before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
+  before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info, :edit_basic_ad, :update_basic_ad]
   before_action :set_one_month, only: :show
 
   def index
@@ -60,13 +60,11 @@ class UsersController < ApplicationController
   end
   
   def edit_basic_ad
-    @user = User.find(params[:id])
+    
   end 
   
   def update_basic_ad
-    @user = User.find(params[:id])
     @users = User.all
-    
     @users.each do |users|
        
       unless users.update_attributes(basic_ad_params)
