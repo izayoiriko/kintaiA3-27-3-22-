fe6139443e7b802f75a3acb027a85597d984ab49
izayoiliko <mmoursnuimneg@gmail.com>
2020-03-21 08:39:37 +0000

    3/21 勤怠編集バリデーション test9\21 完了 追加

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -10,6 +10,7 @@ class Attendance < ApplicationRecord
   validate :started_at_than_finished_at_fast_if_invalid
   
   validate :started_at_only
+  
   # 1
   def finished_at_is_invalid_without_a_started_at
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
@@ -23,16 +24,7 @@ class Attendance < ApplicationRecord
   # 1
   def started_at_only
     if started_at.present? && finished_at.blank?
-      errors.add(:finished_at, "が必要です") if started_at.day < Date.current.day
-    end
-  end
-  
-  def attendances_valid?
-    if started_at.blank? && finished_at.blank?
-    elsif started_at.blank? || finished_at.blank?
-      errors.add(:finished_at, "が必要です")
-    elsif started_at > finished_at
-      errors.add(:started_at, "より早い退勤時間は無効です")
+      errors.add(:finished_at, "が必要です") unless Date.current == worked_on
     end
   end
 end
