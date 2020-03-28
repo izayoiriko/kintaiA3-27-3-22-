03e371157b8fc7462a5e6331ed21f85efe87c7b0
izayoiliko <mmoursnuimneg@gmail.com>
2020-03-28 07:47:24 +0000

    3/28 カラムsuperior 追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,21 @@
 # coding: utf-8
 
-User.create!(name: "Sample User",
+User.create!(name: "管理者",
              email: "sample@email.com",
              password: "password",
              password_confirmation: "password",
              admin: true)
+             
+10.times do |n|
+  name  = Faker::Name.name
+  email = "jotyo-1#{n+1}@email.com"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               superior: true)
+end
              
 60.times do |n|
   name  = Faker::Name.name
