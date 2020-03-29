0fa89494c756f5989e838c86d878e12fe4ac6c01
izayoiliko <mmoursnuimneg@gmail.com>
2020-03-29 21:04:50 +0000

    3/30 department-->affiliation 追加

---
+++ db/migrate/20200329205530_rename_departmemt_column_to_users.rb
@@ -0,0 +1,5 @@
+class RenameDepartmemtColumnToUsers < ActiveRecord::Migration[5.1]
+  def change
+    rename_column :users, :department, :affiliation
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200329002200) do
+ActiveRecord::Schema.define(version: 20200329205530) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -31,9 +31,9 @@ ActiveRecord::Schema.define(version: 20200329002200) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
-    t.string "department"
-    t.datetime "basic_time", default: "2020-03-28 23:00:00"
-    t.datetime "work_time", default: "2020-03-28 22:30:00"
+    t.string "affiliation"
+    t.datetime "basic_time", default: "2020-03-29 23:00:00"
+    t.datetime "work_time", default: "2020-03-29 22:30:00"
     t.boolean "superior", default: false
     t.string "public_uid"
     t.integer "base", default: 1
